4589266dfb81c66e84b3617edbf6c0e1e6d82ee5
Guo Xuanhao <2548110097@qq.com>
2020-03-21 10:24:46 +0800

    update2

--- fileinfo.py
+++ fileinfo.py
@@ -23,3 +23,28 @@ c=c.split()
 for i in a:
     if i not in c:
         d.append(i)
+e=set(d)
+for i in e:
+    print('停用',i,':',d.count(i))
+    x[i]=d.count(i)
+g=x.values()
+g=list(g)
+h=x.keys()
+h=list(h)
+for i in range(6):
+    j=g.index(max(g))
+    print(h[j],max(g))
+    k.append(h[j])
+    l.append(max(g))
+    g.remove(max(g))
+    h.remove(h[j])
+from tkinter import *
+top=Tk()
+top.geometry('300x200')
+top.title('文本编辑器')
+def showoff():
+    root1=Tk()
+    root1.geometry('1000x200')
+    root1.title('文本')
+    label1=Label(root1,text=m,wraplength=1000)
+    label1.pack()
