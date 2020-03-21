9327a8325664dcf50111d2228467699223e29b22
Guo Xuanhao <2548110097@qq.com>
2020-03-21 10:26:54 +0800

    update4

--- fileinfo.py
+++ fileinfo.py
@@ -63,4 +63,23 @@ def statistics2():
     label3=Label(root3,text=y,wraplength=1000)
     label3.pack()
     root3.mainloop()
+    def keyword():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    plt.bar(range(len(l)),l,tick_label=k)
+    plt.title('the words that are used the most often')             
+    plt.show()
+button1=Button(top,text='显示文本',command=showoff)
+button2=Button(top,text='总单词个数统计',command=statistics1)
+button3=Button(top,text='词频统计',command=statistics2)
+button4=Button(top,text='关键词',command=keyword)
+button5=Button(top,text='退出',command=quit)
+button1.grid_configure(column=1,row=1,columnspan=1,rowspan=1)
+button2.grid_configure(column=1,row=3,columnspan=1,rowspan=1)
+button3.grid_configure(column=1,row=5,columnspan=1,rowspan=1)
+button4.grid_configure(column=1,row=7,columnspan=1,rowspan=1)
+button5.grid_configure(column=1,row=9,columnspan=1,rowspan=1)
+top.mainloop()
+
+
 
