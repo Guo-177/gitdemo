dd60645205a389d5e905c7f73715f5250a96819d
Guo Xuanhao <2548110097@qq.com>
2020-03-21 10:22:58 +0800

    update1

--- fileinfo.py
+++ fileinfo.py
@@ -7,3 +7,19 @@ for i in a:
     if i==','or i=='.' or i=='!' or i=='?':
         a.remove(i)
 print(a)
+print('总数:',len(a))
+b=set(a)
+d=[]
+x={}
+y={}
+k=[]
+l=[]
+for i in b:
+    print(i,':',a.count(i))
+    y[i]=a.count(i)
+t=open('d:\\tingyong.txt','r')
+c=t.read()
+c=c.split()
+for i in a:
+    if i not in c:
+        d.append(i)
